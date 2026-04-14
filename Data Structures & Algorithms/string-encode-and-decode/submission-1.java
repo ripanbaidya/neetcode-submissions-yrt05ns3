592a2a7af415f8e862accd7ff9142d2eeeeb774f
Ripan Baidya <116362939+ripanbaidya@users.noreply.github.com>
2026-04-14 11:12:10 +0530

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (String str : strs) {
+            sb.append(str.length()).append("@").append(str);
+        }
+        
+        return sb.toString();
+    }
+
+    // ©hello©world©
+    public List<String> decode(String str) {
+        List<String> result = new ArrayList<>();
+        int i = 0;
+
+        while ( i < str.length()) {
+            int j = i;
+            
+            while (str.charAt(j) != '@') {
+                j ++;
+            }
+
+            int wordLen = Integer.parseInt(str.substring(i, j));
+            String word = str.substring(j+1, j+1+wordLen);
+
+            result.add(word);
+            i = j+1+wordLen;
+        }
+
+        return result;
+    }
+}
